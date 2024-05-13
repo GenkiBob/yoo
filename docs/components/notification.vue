b1f5eea60cf9e890db9794352d622d57386dc267
panny <coocangela@gmail.com>
2024-05-13 18:33:52 +0800

    modify

--- docs/components/notification.vue
+++ docs/components/notification.vue
@@ -5,7 +5,7 @@
     mounted () {
       ElNotification.success({
         title: '推广大使计划2.0全新发布',
-        message: '<a href="https://element-plus.org" target="_blank">点击查看详情</a>',
+        message: '<a href="https://n.tyro.wiki" target="_blank">点击查看详情</a>',
         dangerouslyUseHTMLString: true,
         offset: 100,
         duration: 5000,
